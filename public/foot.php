312fe9e633c3ebf7d494e94b736d371628cf681b
Per Engström <per.olov.engstrom@gmail.com>
2016-01-21 21:55:55 +0100

    Redirected repo to moebius.nu

--- public/foot.php
+++ public/foot.php
@@ -1,6 +1,6 @@
 <div class="footer">
   <div class="container">
     <p class="text-center">&copy; Per Engström</p>
-    <p class="text-center"><a href="https://github.com/pengstrom/pdfprint">v. 2.0.1</a></p>
+    <p class="text-center"><a href="https://git.moebius.nu/perengstrom/pdfprint">v. 2.0.1</a></p>
   </div>
 </div>
