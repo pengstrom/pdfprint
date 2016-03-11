b1f4b1f9e244e21971915a08635aa324e06be35e
Per Engström <per.olov.engstrom@gmail.com>
2016-03-11 17:23:53 +0100

    Implemented error report in api

--- public/api/printers.php
+++ public/api/printers.php
@@ -10,7 +10,7 @@ try {
 
     $printerOptions = (array) json_decode($content = file_get_contents($jsonfile));
     if ($content === false) {
-        throw new \RuntimeException("JSON file not found");
+        throw new \RuntimeException("Printer info file not found");
     }
     $printers = array_keys($printerOptions);
     sort($printers);
@@ -18,8 +18,8 @@ try {
     http_response_code(200);
     echo json_encode($printers);
 } catch (Exception $e) {
-    header("HTTP/1.1 500 OK");
-    echo json_encode([]);
+    http_response_code(500);
+    echo json_encode($e->getMessage());
 }
 
 ?>
