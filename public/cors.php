85d8038bceb0220b1e18cf623af7dfb54996832c
Per Engström <per.olov.engstrom@gmail.com>
2016-07-26 20:25:07 +0200

    Correct Content-Type header

--- public/cors.php
+++ public/cors.php
@@ -6,6 +6,8 @@ header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: content-type');
 header('Access-Control-Max-Age: 86400');
 
+header('Content-Type: application/json');
+
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     exit;
 }
