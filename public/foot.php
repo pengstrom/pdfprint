9e8d768aeb29e0608f39e0f1b0cf35086a1e4599
Per Engström <per.olov.engstrom@gmail.com>
2016-08-09 09:36:33 +0200

    Incremented footer version

--- public/foot.php
+++ public/foot.php
@@ -1,7 +1,7 @@
 <div class="footer">
   <div class="container">
     <p class="text-center">&copy; Per Engström</p>
-    <p class="text-center"><a href="https://git.moebius.nu/perengstrom/pdfprint">v. 2.0.7</a></p>
+    <p class="text-center"><a href="https://git.moebius.nu/perengstrom/pdfprint">v. 3.0.1</a></p>
 
     <p class="text-center">Om du stöter på problem, gör en anmälan <a href="https://github.com/pengstrom/pdfprint/issues">här.</a></p>
   </div>
